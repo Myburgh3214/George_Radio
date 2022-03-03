651b62e5ebcfb3adca0a39ad093e7882dca095a8
Myburgh3214 <79691187+Myburgh3214@users.noreply.github.com>
2022-03-03 11:40:37 +0200

    Add files via upload

---
+++ george_radiostation/frontend/js/script.js
@@ -0,0 +1,24 @@
+function homePge() {
+    location.replace("./index.html")
+  }
+  function aboutBtn() {
+    location.replace("./about.html")
+  }
+  function djList() {
+    location.replace("./dj_page.html")
+  }
+  function eventsBtn() {
+    location.replace("./events.html")
+  }
+  
+  function tuneInbtn() {
+
+    // Simulate an HTTP redirect:
+    window.location.replace("http://listen.samcloud.com/w/127315/station-1#history");  }
+    function logIn() {
+
+      // Simulate an HTTP redirect:
+      window.location.replace("http://listen.samcloud.com/w/127315/station-1#history");  }
+  
+  
+          
